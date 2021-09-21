d970ba3409406ff7400561df0e4d186aa1fb1c1c
Roberto Bertelli <robbybertelli@gmail.com>
2021-09-21 19:35:37 +0200

    Add check if your shift is inside in a longer shift
    Signed-off-by: Roberto Bertelli <robbyberteli@gmail.com>

--- puny.py
+++ puny.py
@@ -7,6 +7,7 @@ from selenium.webdriver.firefox.options import Options
 import datetime
 from datetime import date
 import calendar
+import re
 
 options = Options()
 options.headless = True
@@ -18,30 +19,47 @@ user = data["credentials"]["username"]
 password = data["credentials"]["password"]
 today = data["calendar"][today_str]
 
+
+def strToHour(str):
+    return datetime.datetime.strptime(str, "%H:%M")
+
+
 for c in today:
     building = c["building"]
     classroom = c["classroom"]
-    shift = c["shift"]
+    shift_start = strToHour(c["shift_start"])
+    shift_end = strToHour(c["shift_end"])
     with webdriver.Firefox() as driver:
-        wait = WebDriverWait(driver, 10)
         driver.get("https://www.unimore.it/covid19/trovaaula.html")
         # accept cookies
         driver.find_element_by_xpath("/html/body/div[3]/p/a[1]").click()
-        time.sleep(0.5)
+        time.sleep(2)
         # choosing the building
         driver.find_element_by_xpath(
             f"/html/body/div[2]/div[3]/main//li[contains(text(), '{building}')]/a[1]").click()
+        time.sleep(2)
         # choosing the classroom and shift
-        driver.find_element_by_xpath(
-            f"//td[contains(text(), '{classroom}')]/parent::tr/td[4]//a[contains(text(), '{shift}')]").click()
-        time.sleep(0.5)
+        shifts = driver.find_elements_by_xpath(
+            f"//td[contains(text(), '{classroom}')]/parent::tr/td[4]//a")
+
+        found = False
+        for s in shifts:
+            hours = re.findall("\\d{2}:\\d{2}", s.text)
+            start = strToHour(hours[0])
+            end = strToHour(hours[1])
+            if (start <= shift_start) and (end >= shift_end):
+                found = True
+                s.click()
+        if not found:
+            print("can't find your shift")
+
+        time.sleep(2)
         # login
         driver.find_element_by_xpath("//*[@id='username']").send_keys(user)
         driver.find_element_by_xpath("//*[@id='password']").send_keys(password)
         driver.find_element_by_xpath(
             "/html/body/div/div/div/div[1]/form/div[4]/button").click()
         # confirm reservation
-        time.sleep(0.5)
+        time.sleep(2)
         driver.find_element_by_xpath(
             "/html/body/div[3]/div[4]/form/div/div/p[8]/button").click()
-        input()
